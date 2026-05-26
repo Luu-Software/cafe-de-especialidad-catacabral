199e45bcb387c590306354dffefe6d37a5275bbd
CatalinaCabral <catalinaisabelcabral@gmail.com>
2026-05-26 12:11:39 -0300

    Empecé

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,8 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
+let total:number= Number(totalTexto);
+let estudiantes:number=Number(esEstudiante)
+let personas: number=Number(personasTexto)
 let totalPorPersona: number = 0; // COMPLETAR
 console.log(`Total por persona: $${totalPorPersona}`);
